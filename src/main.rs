ec1f9d42a7c2d9f71373074d7c1cfb6fb5da241c
Sean <sd.cotter45@gmail.com>
2023-10-15 20:48:09 -0400

    added swagger docs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::fs::File;
 use std::io::BufReader;
-use serde::{Deserialize, Serialize};
 use actix_web::web::Data; 
+use utoipa::{ToSchema, OpenApi};
+use utoipa_swagger_ui::SwaggerUi;
+use serde::{Deserialize, Serialize};
 use actix_web::{web, App, HttpServer, HttpResponse, Responder, get};
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
+
+#[derive(Debug, Deserialize, Serialize, Clone, ToSchema)]
 #[serde(rename_all = "camelCase")]
 struct State {
     id: u32,
@@ -14,7 +17,7 @@ struct State {
     shared_parks: Option<u32>,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)] 
+#[derive(Debug, Deserialize, Serialize, Clone, ToSchema)] 
 #[serde(rename_all = "camelCase")]
 struct Park {
     id: u32,
@@ -25,6 +28,12 @@ struct Park {
     area: String,
     visitors: u32,
     description: String,
+}
+
+#[derive(Serialize, Deserialize, ToSchema)]
+struct Response {
+    pub status: &'static str,
+    pub message: String,
 }
 
 struct AppState {
@@ -40,6 +49,27 @@ impl AppState {
         }
     }
 }
+
+#[derive(OpenApi)]
+#[openapi(
+    paths(
+        get_all_parks,
+        get_park_by_id,
+        find_parks_by_query,
+        get_all_states,
+        get_state_by_id,
+        find_states_by_query,
+    ),
+    components(
+        schemas(Response, Park, State)
+    ),
+    tags(
+        (name = "Parks", description = "National Parks"),
+        (name = "States", description = "States with National Parks"),
+    ),
+)]
+struct ApiDoc;
+
 
 fn load_parks() -> Vec<Park> {
     let file = File::open("./data/parks.json").expect("Failed to open parks.json");
@@ -76,21 +106,52 @@ fn fuzzy_match(query: &str, target: &str) -> bool {
     false
 }
 
+#[utoipa::path(
+    get,
+    path = "/parks",
+    tag = "Parks",
+    responses(
+        (status = 200, description= "Get all parks", body = Vec<Park>),
+        (status = 500, description= "Internal server error", body = Response),
+    )
+)]
 #[get("/parks")]
 async fn get_all_parks(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok().json(&data.parks)
 }
 
+#[utoipa::path(
+    get,
+    path = "/parks/{id}",
+    tag = "Parks",
+    responses(
+        (status = 200, description= "Get park by ID", body = Park),  
+        (status = 404, description= "Park not found", body = Response),  
+        (status = 500, description= "Internal server error", body = Response), 
+    )
+)]
 #[get("/parks/{id}")]
 async fn get_park_by_id(info: web::Path<u32>, data: web::Data<AppState>) -> impl Responder {
     let id: u32 = info.into_inner();
     if let Some(park) = data.parks.iter().find(|p| p.id == id) {
         HttpResponse::Ok().json(park)
     } else {
-        HttpResponse::NotFound().finish()
+        HttpResponse::NotFound().json(Response {
+            status: "error",
+            message: "Park not found".to_string(),
+        })
     }
 }
 
+#[utoipa::path(
+    get,
+    path = "/parks/search/{query}",
+    tag = "Parks",
+    responses(
+        (status = 200, description= "Find parks by query", body = Vec<Park>),   
+        (status = 500, description= "Internal server error", body = Response),  
+    )
+)]
 #[get("/parks/search/{query}")]
 async fn find_parks_by_query(info: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
     let query = info.into_inner();
@@ -103,21 +164,52 @@ async fn find_parks_by_query(info: web::Path<String>, data: web::Data<AppState>)
     HttpResponse::Ok().json(results)
 }
 
+#[utoipa::path(
+    get,
+    path = "/states",
+    tag = "States",
+    responses(
+        (status = 200, description= "Get all states", body = State),
+        (status = 500, description= "Internal server error", body = Response), 
+    )
+)]
 #[get("/states")]
 async fn get_all_states(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok().json(&data.states)
 }
 
+#[utoipa::path(
+    get,
+    path = "/states/{id}",
+    tag = "States",
+    responses(
+        (status = 200, description= "Get state by ID", body = State),
+        (status = 404, description= "State not found", body = Response),
+        (status = 500, description= "Internal server error", body = Response),   
+    )
+)]
 #[get("/states/{id}")]
 async fn get_state_by_id(info: web::Path<u32>, data: web::Data<AppState>) -> impl Responder {
     let id: u32 = info.into_inner();
     if let Some(state) = data.states.iter().find(|s| s.id == id) {
         HttpResponse::Ok().json(state)
     } else {
-        HttpResponse::NotFound().finish()
+        HttpResponse::NotFound().json(Response {
+            status: "error",
+            message: "State not found".to_string(),
+        })
     }
 }
 
+#[utoipa::path(
+    get,
+    path = "/states/search/{query}",
+    tag = "States",
+    responses(
+        (status = 200, description= "Find states by query", body = Response), 
+        (status = 500, description= "Internal server error", body = Response),    
+    )
+)] 
 #[get("/states/search/{query}")]
 async fn find_states_by_query(info: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
     let query = info.into_inner();
@@ -132,7 +224,9 @@ async fn find_states_by_query(info: web::Path<String>, data: web::Data<AppState>
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let openapi = ApiDoc::openapi();
+
+    HttpServer::new(move || {
         App::new()
             .app_data(Data::new(AppState::new()))
             .service(get_all_parks)
@@ -141,6 +235,9 @@ async fn main() -> std::io::Result<()> {
             .service(get_all_states)
             .service(get_state_by_id)
             .service(find_states_by_query)
+            .service(
+                SwaggerUi::new("/{_:.*}").url("/api-docs/openapi.json", openapi.clone()),
+            )
     })
     .bind(("0.0.0.0", 8080))?
     .run()
